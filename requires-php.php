6a0fd86de4d01dba7387510ecbfb7bdcc22e229d
Andy Fragen <andy@thefragens.com>
2018-03-30 07:57:37 -0700

    rename functions more descriptively

--- requires-php.php
+++ requires-php.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Requires PHP
  * Plugin URI:        https://github.com/afragen/requires-php/
  * Description:       This plugin is used for testing.
- * Version:           0.4.0
+ * Version:           0.4.1
  * Author:            Andy Fragen
  * License:           MIT
  * License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -25,18 +25,18 @@ class Requires_PHP {
 	 * Requires_PHP constructor.
 	 */
 	public function __construct() {
-		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'pre_set_site_transient_update_plugins', ) );
-		add_filter( 'upgrader_pre_download', array( $this, 'upgrader_pre_download' ) );
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'unset_update_plugins_transient', ) );
+		add_filter( 'upgrader_pre_download', array( $this, 'exit_add_plugin_process' ) );
 	}
 
 	/**
-	 * Unset update transient as appropriate.
+	 * Unset update_plugins transient as appropriate.
 	 *
 	 * @param object $transient Update transient.
 	 *
 	 * @return object $transient Update transient.
 	 */
-	public function pre_set_site_transient_update_plugins( $transient ) {
+	public function unset_update_plugins_transient( $transient ) {
 		foreach ( (array) $transient->response as $update ) {
 			if ( ! $this->is_required_php( $update ) ) {
 				unset( $transient->response[ $update->plugin ] );
@@ -51,10 +51,10 @@ class Requires_PHP {
 	 *
 	 * @return \WP_Error|bool
 	 */
-	public function upgrader_pre_download() {
+	public function exit_add_plugin_process() {
 		$repo = (object) $_POST;
 		if ( ! $this->is_required_php( $repo ) ) {
-			return new \WP_Error( 'requires_php', __( 'Upgrade PHP version to install this plugin.' ) );
+			return new \WP_Error( 'requires_php', __( 'Upgrade PHP to install this plugin.' ) );
 		}
 
 		return false;
@@ -68,7 +68,7 @@ class Requires_PHP {
 	 * @return bool
 	 */
 	protected function is_required_php( $repo ) {
-		$response = $this->get_dot_org_api_data( $repo->slug );
+		$response = $this->get_plugin_dot_org_api_data( $repo->slug );
 
 		return ( isset( $response->requires_php ) && version_compare( PHP_VERSION, $response->requires_php, '>=' ) );
 	}
@@ -80,7 +80,7 @@ class Requires_PHP {
 	 *
 	 * @return object|bool $response
 	 */
-	protected function get_dot_org_api_data( $slug ) {
+	protected function get_plugin_dot_org_api_data( $slug ) {
 		$url      = 'https://api.wordpress.org/plugins/info/1.1/';
 		$url      = add_query_arg( array(
 			'action'        => 'plugin_information',
