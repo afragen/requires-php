6ab55e98cba6270bf14751dd2e0715e253a566b8
Andy Fragen <andy@thefragens.com>
2018-03-31 10:25:07 -0700

    fix return for API 1.2

--- requires-php.php
+++ requires-php.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Requires PHP
  * Plugin URI:        https://github.com/afragen/requires-php/
  * Description:       This plugin is used for testing.
- * Version:           0.4.2
+ * Version:           0.4.4
  * Author:            Andy Fragen
  * License:           MIT
  * License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -69,7 +69,8 @@ class Requires_PHP {
 	protected function is_required_php( $slug ) {
 		$response = $this->get_plugin_dot_org_api_data( $slug );
 
-		return ( isset( $response->requires_php ) && version_compare( PHP_VERSION, $response->requires_php, '>=' ) );
+		return isset( $response->error ) ||
+		       ( isset( $response->requires_php ) && version_compare( PHP_VERSION, $response->requires_php, '>=' ) );
 	}
 
 	/**
